e1b0afdb5858737a35115a89d53104641b980504
onesword0618 <ao.akua.leo@gmail.com>
2018-08-18 23:18:58 +0900

    四則演算処理の実装_単純処理

---
+++ prairie_dogs/src/main/Calculator.java
@@ -0,0 +1,53 @@
+package main;
+
+/**
+ * 計算処理
+ * 
+ * @author onesword0618
+ */
+public class Calculator {
+
+	/**
+	 * 加算処理
+	 * 
+	 * @param x
+	 * @param y
+	 * @return 加算結果
+	 */
+	public int pluss(int x, int y) {
+		return x + y;
+	}
+
+	/**
+	 * 減算処理
+	 * 
+	 * @param x
+	 * @param y
+	 * @return 減算結果
+	 */
+	public int minus(int x, int y) {
+		return x - y;
+	}
+
+	/**
+	 * 乗算処理
+	 * 
+	 * @param x
+	 * @param y
+	 * @return 乗算結果
+	 */
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+
+	/**
+	 * 除算処理
+	 * 
+	 * @param x
+	 * @param y
+	 * @return 除算結果
+	 */
+	public int divided(int x, int y) {
+		return x / y;
+	}
+}
